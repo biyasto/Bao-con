7df320a144145fbe21a8689129f4067e0d9f51c0
biyasto <58619678+biyasto@users.noreply.github.com>
2022-11-06 11:58:04 +0000

    update msg

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require("express"); 
 const app = express();
 
@@ -6,21 +5,62 @@ app.listen(3000, () => {
   console.log("project is running!");
 })
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
-})
+
 
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildPresences,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessageReactions,
   ],
 });
 
 
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+const agents = ["Astra", "Breach", "Brimstone", "Chamber", "Cypher","Habor", "Jett", "KAY/O", "Killjoy","Neon","Omen","Phoenix","Raze","Reyna","Sage","Skye","Sova","Viper","Yoru"];
 
 
- 
 
+client.on('messageCreate', (message) => {
+    if (message.content.includes("con gi")|| message.content.includes("con gì")){
+      const random = Math.floor(Math.random() * agents.length);
+        message.reply(agents[random])
+    }
+});
+
+
+const khen = ["Gke","10 điểm","10k momo","chòi oi! ghê quá"];
+client.on('messageCreate', (message) => {
+    if (message.content.includes("ace")){
+      const random = Math.floor(Math.random() * khen.length);
+        message.reply(khen[random])
+    }
+});
+
+const ban = ["Bùm chíu","pằng pằng","🔫🔫"];
+client.on('messageCreate', (message) => {
+    if (message.content.includes("ban") || message.content.includes("bắn")){
+      const random = Math.floor(Math.random() * ban.length);
+        message.reply(ban[random])
+    }
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content.includes("invite")){
+     message.reply("https://discord.gg/UShzKuv9")
+    }
+});
+
+client.on('messageCreate', (message) => {
+    if (message.content.includes("Tô") && message.content.includes("Đạt")){
+     message.reply("☠️☠️☠️")
+    }
+});
 client.login(process.env.token)
